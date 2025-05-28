d64240b041bb69ee12f52ac15a63ef14da226eae
Karl Adrian <karladrian995@gmail.com>
2025-05-28 21:31:07 +0300

    polish room list

--- frontend/hostelbooking/components/RoomList.tsx
+++ frontend/hostelbooking/components/RoomList.tsx
@@ -2,14 +2,13 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { FaStar, FaStarHalf } from "react-icons/fa";
 
 const RoomList = ({ rooms }: { rooms: any }) => {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       {rooms.data.map((room: any) => {
-        console.log(room);
         const imgURL = `http://127.0.0.1:1337${room.image?.url}`;
-
         return (
           <div key={room.id}>
             <Link href={`/room/${room.id}`}>
@@ -24,6 +23,25 @@ const RoomList = ({ rooms }: { rooms: any }) => {
                 />
               </div>
             </Link>
+            <div className="h-[134px]">
+              <div className="flex items-center justify-between mb-6">
+                <div>Capacity: {room.capacity} person</div>
+                <div className="flex gap-1 text-accent">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalf />
+                </div>
+              </div>
+              <Link href={`/room/${room.id}`}>
+                <h3 className="h3">{room.title}</h3>
+              </Link>
+              <p className="h3 font-secondary font-medium text-accent mb-4">
+                ${room.price}
+                <span className="text-base text-secondary">/ night</span>
+              </p>
+            </div>
           </div>
         );
       })}
--- frontend/hostelbooking/tailwind.config.ts
+++ frontend/hostelbooking/tailwind.config.ts
@@ -1,20 +1,50 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      primary: "--font-marcellus",
+      secondary: "--font-urbanist",
+    },
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#292836",
+          hover: "#3e3d4a",
+        },
+        secondary: {
+          DEFAULT: "#6b6a71",
+        },
+        accent: {
+          DEFAULT: "#e85f4c",
+          hover: "#ea6f5e",
+        },
+        tertiary: {
+          DEFAULT: "#faf5ef",
+        },
+        grey: {
+          DEFAULT: "#a09faf",
+        },
+      },
+
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;
