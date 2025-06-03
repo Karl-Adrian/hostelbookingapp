d2926a357ca86e044ee07a1004f16847a3d49563
Karl Adrian <karladrian995@gmail.com>
2025-06-03 15:42:18 +0300

    changes lol

--- frontend/hostelbooking/app/room/[id]/page.tsx
+++ frontend/hostelbooking/app/room/[id]/page.tsx
@@ -1,4 +1,20 @@
-const RoomDetails = () => {
+const getRoomData = async ({ params }: { params: any }) => {
+  const res = await fetch(
+    `http://localhost:1337/api/rooms/${params.id}?populate=*`,
+    {
+      next: {
+        revalidate: 0,
+      },
+    }
+  );
+  return await res.json();
+};
+
+const RoomDetails = async ({ params }: { params: any }) => {
+  const room = await getRoomData({ params });
+  console.log(room);
+  const imgURL = `http://localhost:1337${room.data?.image.data.url}`;
+  console.log(imgURL);
   return <div>Room Details</div>;
 };
 
